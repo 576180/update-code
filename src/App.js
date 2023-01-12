71c5a1b07106a4f79dcc1266f529ce789fe98883
Mehul Panchal <mepanchal12@gmail.com>
2023-01-13 01:15:19 +0530

    1 st part changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='h1'>
+      helllo  every one my name is mehul panchakl and i from the banswar wwaht you ant yto do n ext and mkkre 
+    </div>
+  );
+}
+
+export default App;
